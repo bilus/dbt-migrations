efeab5f0c942d08bb6b1a17f133a796fa17ffc41
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-03 14:57:19 +0800

    Delete macros directory

--- macros/enforce_migration_dependancy.sql
+++
@@ -1,8 +0,0 @@
-{% macro enforce_migration_dependancy() -%}
-{% set help_dbt_infer_dependancies = ref('change_history') %}
-{% set this_rev = (this.name | int) %}
-{% if this_rev > 1 %}
-    {% set prev_rev = (this_rev-1)| string %}
-    {% set x = ref(prev_rev) %}
-{% endif %}
-{%- endmacro %}
